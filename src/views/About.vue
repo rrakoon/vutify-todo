74e620ec03fa02b606d41c9598dfe4c97e6b7140
rrakoon <lth0285@gmail.com>
2021-02-26 18:50:28 +0900

    vuetify todo

---
+++ src/views/About.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about pa-12">
+    <h1>About Vuetify Todo</h1>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat repellat reprehenderit explicabo deserunt ab vero quis, laborum iste possimus accusamus delectus corporis aspernatur facilis voluptates, perferendis accusantium totam tenetur dolores.
+    </p>
+    
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat repellat reprehenderit explicabo deserunt ab vero quis, laborum iste possimus accusamus delectus corporis aspernatur facilis voluptates, perferendis accusantium totam tenetur dolores.
+    </p>
+    
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat repellat reprehenderit explicabo deserunt ab vero quis, laborum iste possimus accusamus delectus corporis aspernatur facilis voluptates, perferendis accusantium totam tenetur dolores.
+    </p>
+  </div>
+</template>
